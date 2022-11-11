a1490bfc9d06aa3dac4716f0b67cd833d7e4d248
bilal aymak <bilalaymak@gmail.com>
2022-11-11 03:46:36 +0300

    second commit

--- src/testclassbilalaymak.java
+++ src/testclassbilalaymak.java
@@ -2,5 +2,7 @@ public class testclassbilalaymak {
     public static void main(String[] args) {
         System.out.println("Git den merhaba!");
         System.out.println("GitHub dan merhaba!");
+        System.out.println("GitHub a bir diğer komut");
+
     }
 }
