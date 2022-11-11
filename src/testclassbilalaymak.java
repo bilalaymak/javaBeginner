08787e908cedbf677a72be51a2b595702e8d2bdc
bilalaymak <117953831+bilalaymak@users.noreply.github.com>
2022-11-11 03:26:01 +0300

    Update testclassbilalaymak.java

--- src/testclassbilalaymak.java
+++ src/testclassbilalaymak.java
@@ -1,5 +1,6 @@
 public class testclassbilalaymak {
     public static void main(String[] args) {
         System.out.println("Git den merhaba!");
+        System.out.println("GitHub dan merhaba!");
     }
 }
