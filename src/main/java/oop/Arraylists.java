a8a5a0bdb27a4f6c7cdaf561c1b9be3255386f62
bilalaymak <bilalaymak@gmail.com>
2022-12-15 06:39:06 +0300

    güncelleme

---
+++ src/main/java/oop/Arraylists.java
@@ -0,0 +1,9 @@
+package oop;
+
+public class Arraylists {
+    public static void main(String[] args) {
+
+
+
+    }
+}
---
+++ src/main/java/oop/MultidimensionalArrays.java
@@ -0,0 +1,47 @@
+package oop;
+
+public class MultidimensionalArrays {
+    public static void main(String[] args) {
+
+
+        int[][] arr = {{10,20,30},{40,50,60}};
+
+        System.out.println("0'a 0 = " + arr[0][0]);
+        System.out.println("1'e 0 = " + arr[1][0]);
+
+        System.out.println("*********************");
+
+        int[][] arr2 = new int[2][3];
+        arr2[0][0] = 10;
+        arr2[0][1] = 20;
+        arr2[0][2] = 30;
+        arr2[1][0] = 40;
+        arr2[1][1] = 50;
+        arr2[1][2] = 60;
+
+        for (int i=0; i<2; i++){
+            for (int j = 0; j< 3; j++){
+                System.out.println("eleman = " + arr2[i][j]);
+            }
+            /*
+             arr2[i][j] ifadesi, iç içe geçmiş iki döngünün
+             sayısı kadar tekrarlanarak çok boyutlu bir dizideki tüm elemanlara erişmenizi sağlar.
+             */
+
+        }
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+}
